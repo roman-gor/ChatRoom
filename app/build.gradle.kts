e0227d063473c78748c3dd0c2f176e2662b3a14e
Roman <romangorbachev2006@gmail.com>
2026-01-03 15:51:33 +0300

    Setting up Firebase App Distribution, CD for app deploying

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,7 @@ android {
         release {
             isMinifyEnabled = true
             isShrinkResources = true
+            signingConfig = signingConfigs.getByName("release")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
